b4260ac47e927084ab74fbd90247ddd2d6756dde
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2018-05-16 09:30:49 +0200

    Commit git

---
+++ src/exemplotiposlambda/ExemploTiposLambda.java
@@ -0,0 +1,19 @@
+
+package exemplotiposlambda;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class ExemploTiposLambda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Persoa obx=new Persoa();
+        obx.crear();
+        obx.ver();
+    }
+    
+}
